edf458c70569f83fcc017b9be4b07a19f7f176d7
DreamLuke <36737163+DreamLuke@users.noreply.github.com>
2019-02-25 10:43:07 +0200

    Add files via upload

--- addCar.php
+++ addCar.php
@@ -3,16 +3,18 @@ $id = $_GET['id'] ?? '';
 
 $carData = ['id'=> '', 'name' => '', 'manufacture_year'=>'', 'city_id'=>''];
 
-if(!empty($id))
-{
+/*if(!empty($id))
+{*/
     include_once $_SERVER['DOCUMENT_ROOT'].'/models/cars.php';
 
     $carsClass = new Cars();
+    $cars = $carsClass->get_all();
+
     $result = $carsClass->get_one(['id' => $id]);
     if($result){
         $carsClass = $result;
     }
-}
+/*}*/
 
 ?>
 <!doctype html>
@@ -26,11 +28,12 @@ if(!empty($id))
 </head>
 <body>
 
+<h1>Добавить автомобиль</h1>
 <form action="/controllers/save.php?operator=addcar" method="post">
-    <p>
+    <!-- <p>
         <label for="id">id</label>
         <input type="number" name="id" id="id" value="<?php echo $carData['id']?>">
-    </p>
+    </p> -->
     <p>
         <label for="">Название машины</label>
         <input type="text" name="name" value="<?php echo $carData['name']?>">
@@ -44,10 +47,17 @@ if(!empty($id))
         <input type="number" name="city_id" value="<?php echo $carData['city_id']?>">
     </p>
     <p>
-        <!-- <input type="hidden" name="id" value="<?php echo $carData['id']?>"> -->
+        <input type="hidden" name="id" value="<?php echo $carData['id']?>">
         <button>Сохранить</button>
     </p>
 </form>
+
+<h1>Список автомобилей</h1>
+<?php foreach ($cars as $car):?>
+<p><?php echo 'id = ' . $car['id'] . '; '?><?php echo 'name = ' . $car['name'] . '; '?> <?php echo 'год выпуска = ' . $car['manufacture_year']?>
+    <a href="/edit.php?id=<?php echo $car['id']; ?>">Редакитровать </a>
+</p>
+<?php endforeach;?>
 
 </body>
 </html>
